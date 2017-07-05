6f82050b17671df28e7369d87ec045823d772491
satoshi.yokoi <yokoi.satoshi@lancers.co.jp>
2017-07-05 13:18:43 +0900

    240固定なので、サーバサイドで直接指定

--- app/controllers/icons_controller.rb
+++ app/controllers/icons_controller.rb
@@ -3,14 +3,15 @@ class IconsController < ApplicationController
   def create
     file_name = params[:image].original_filename.downcase
     mime_type = params[:image].content_type.downcase
+
     if !!file_name.match(/png|jpeg|jpg|gif/) and mime_type.start_with? 'image/'
       dest_file_name = "#{SecureRandom.uuid}#{File.extname(file_name)}"
       image_path = "#{Rails.root}/public/icons/#{dest_file_name}"
       FileUtils.mv params[:image].tempfile, image_path
       FileUtils.chmod 0644, image_path
-      if px = params[:resize_max_pixel]
-        `convert -resize #{px}x#{px} #{image_path} #{image_path}` # ImageMagickで画像をリサイズ
-      end
+
+      `convert -resize 240x240 #{image_path} #{image_path}` # ImageMagickで画像をリサイズ
+
       render json: {file_name: dest_file_name} and return
     else
       render json: {errors: ['画像のアップロードに失敗しました']}, status: :bad_request and return
